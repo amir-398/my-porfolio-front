bc8460bc0f216631ec0dd621d652e982c7bc0b81
amir-398 <amir.398@hotmail.fr>
2024-03-20 16:39:06 +0100

    Mise à jour de la section de présentation avec des modifications de texte

--- app/(pages)/home/components/presentationSection/PresentationSection.tsx
+++ app/(pages)/home/components/presentationSection/PresentationSection.tsx
@@ -15,22 +15,22 @@ export default function PresentationSection() {
             </p>
             <br />
             <p>
-              Avec un arsenal de compétences en constante expansion, j'ai plongé
-              profondément dans l'univers du développement web, armé d'une
-              connaissance avancée dans une multitude de langages de
+              Avec un arsenal de compétences en constante expansion, j&apos;ai
+              plongé profondément dans l&apos;univers du développement web, armé
+              d&apos;une connaissance avancée dans une multitude de langages de
               programmation. Mon parcours est pavé de curiosité insatiable et
-              d'une soif d'apprendre, me poussant toujours à explorer les
-              confins des nouvelles technologies
+              d&apos;une soif d&apos;apprendre, me poussant toujours à explorer
+              les confins des nouvelles technologies
             </p>
             <br />
             <p>
               Je me distingue par ma capacité à déchiffrer les énigmes les plus
               complexes, cherchant toujours à conjuguer efficacité et élégance
               dans chaque solution apportée. Mais au-delà du code, je suis un
-              fervent croyant dans le pouvoir de la communication et de l'esprit
-              d'équipe. La collaboration est à mes yeux l'étoile polaire,
-              guidant vers la matérialisation d'idées innovantes et la
-              réalisation de projets ambitieux.
+              fervent croyant dans le pouvoir de la communication et de
+              l&apos;esprit d&apos;équipe. La collaboration est à mes yeux
+              l&apos;étoile polaire, guidant vers la matérialisation
+              d&apos;idées innovantes et la réalisation de projets ambitieux.
             </p>
           </div>
           <div className={style.right}>
